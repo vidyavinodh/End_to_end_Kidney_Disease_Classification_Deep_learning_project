ea1ccd202fdc0ce5b7c265d6ab2b2c4dfb81eaa7
Vidya Chandrasekar <vidyavinodh1982@gmail.com>
2026-05-07 09:55:24 -0600

    Updated project workflow

---
+++ src/cnnClassifier/utils/common.py
@@ -0,0 +1,40 @@
+import os
+from box.exceptions import BoxValueError
+import yaml
+from cnnClassifier import logger
+import json
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+import base64
+
+
+@ensure_annotations
+def read_yaml(path_to_yaml:Path)->ConfigBox:
+    try:
+        with open(path_to_yaml,"r") as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"Yaml file loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
+
+@ensure_annotations
+def create_directories(path_to_directories:list,verbose=True):
+    for path in path_to_directories:
+        os.makedirs(path,exist_ok=True)
+        if verbose:
+            logger.info(f"Created Directory:{path}")
+
+@ensure_annotations
+def save_json(path:Path,data:dict):
+    with open(path,"w") as f:
+        json.dump(data,f,indent=4)
+    logger.info(f"json file saved:{path}")
+
+
+
+    
