af4c4f25d116a8090ba3c828a5c988be2d6c0110
Vidya Chandrasekar <vidyavinodh1982@gmail.com>
2026-05-21 11:22:00 -0600

    Prediction added

---
+++ src/cnnClassifier/pipeline/prediction.py
@@ -0,0 +1,46 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import os
+
+
+class PredictionPipeline:
+
+    def __init__(self, filename):
+        self.filename = filename
+
+
+    def predict(self):
+
+        # Load trained model
+        model = load_model(
+            os.path.join("artifacts", "training", "model.h5")
+        )
+
+        # Load image
+        test_image = image.load_img(
+            self.filename,
+            target_size=(224, 224)
+        )
+
+        # Convert image to array
+        test_image = image.img_to_array(test_image)
+
+        # Normalize image
+        test_image = test_image / 255.0
+
+        # Expand dimensions
+        test_image = np.expand_dims(test_image, axis=0)
+
+        # Prediction
+        result = np.argmax(model.predict(test_image), axis=1)
+
+        print(result)
+
+        # Class labels
+        if result[0] == 1:
+            prediction = "Tumor"
+        else:
+            prediction = "Normal"
+
+        return [{"image": prediction}]
--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -36,7 +36,16 @@ def save_json(path:Path,data:dict):
     logger.info(f"json file saved:{path}")
 
 
+@ensure_annotations
+def decodeImage(imgstring,fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, "wb") as f:
+        f.write(imgdata)
+        f.close()
 
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath,"rb") as f:
+        return base64.b64encode(f.read())
 
 
     
